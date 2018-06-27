097e5b2f090f339a09f3dead397163562431e11f
mthacker801 <mtvanguard@gmail.com>
2018-06-27 15:36:17 -0600

    Resume
    Resume

---
+++ app.js
@@ -0,0 +1,16 @@
+// const skillButton = document.querySelector('button').addEventListener("click", displayContent);
+
+
+// $('.button').click(function() {
+//   var current = $('.content').index($(this)) + 1;
+//   $('.content').removeClass('active');
+//   $('.content').eq(current).addClass('active');
+// });
+
+var display = function() {
+  $('.button').click(function() {
+    $('.content').toggle;
+  })
+};
+
+$(document).ready(current);
